dfa028238cd61bc15e07b20327bfc2ea2812aed3
Moran <moran@quesnel.com>
2020-09-09 09:15:04 +0200

    add pokemon

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import img from "../img/kouign_amann.jpg"
 import '../scss/style.scss'
+import axios from 'axios';
+import Request from 'axios-react';
 
 export default class App extends Component{
 	constructor(){
@@ -10,23 +12,38 @@ export default class App extends Component{
 		//Etat
 		this.state = {
 			message: 'Hello World !',
-		}
+			pokemon: []
+		};
+		
 	}
 
 	//Comportement
 	handleMessage(){
-		this.setState({message: 'Coucou les gens!'})
+		this.setState({message: 'Les 5 pokémons'})
+		this.getPokemon(5)
+	}
+
+	getPokemon(number) {
+		axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=`+number)
+		.then(res => {
+		  const pokemon = res.data.results;
+		  this.setState({ pokemon });
+		})
 	}
 
 	//Rendu
 	render(){
 		let {message} = this.state;
-	
-		return (
+		let {pokemon} = this.state;
+		return (			
 			<div>
 				<img src={img} alt="Une image bretonne" />
 				<h1>{message}</h1>
 				<button onClick={this.handleMessage.bind(this)}>Changer de message</button>
+
+				<ul>
+					{ pokemon.map(pokemon => <li>{pokemon.name}</li>)}
+				</ul>
 			</div>
 		)
 	}
